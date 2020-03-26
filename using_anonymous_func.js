eca50bb69f593d623018f8eb4fde9f50304b9481
David Mahbubi <ulrichdavid0370@gmail.com>
2020-03-26 21:46:37 +0700

    init

---
+++ using_anonymous_func.js
@@ -0,0 +1,61 @@
+// Anonymous function version
+
+const binCollectionGenerator = lim => {
+
+  let binCollection = new Array();
+
+  for (let i = 1; i <= lim; i++) {
+    if (i === 1) {
+      binCollection.push(i);
+    } else {
+      binCollection.push(binCollection[i - 2] * 2);
+    }
+  }
+
+  return binCollection.reverse();
+}
+
+const decimalToBinaryConverter = number => {
+
+  const binCollection = binCollectionGenerator(8);
+
+  return binCollection.map( num => {
+    if ((number - num) < 0) {
+      return '0';
+    } else {
+      number -= num;
+      return '1';
+    }
+  }).join('');
+
+}
+
+const binaryToDecimalConverter = number => {
+
+  const binLength = 8;
+  const binCollection = binCollectionGenerator(binLength);    
+
+  return number.toString().split('').map( (num, i) => {
+    if ( num == 1 ) {
+      return binCollection[i]
+    } else {
+      return 0;
+    }
+  }).reduce((acc, curr) => acc + curr);
+}
+
+const convertIpAddress = ({ip, type}) => {
+
+  ip = ip.split('.');
+
+  if (type == 'binary') {
+    return ip.map( num => binaryToDecimalConverter(num)).join('.');
+  } else {
+    return ip.map( num => decimalToBinaryConverter(num)).join('.');
+  }
+}
+
+console.log(convertIpAddress({
+  ip: '11000000.10101000.00000001.00000001',
+  type: 'binary'
+}));
